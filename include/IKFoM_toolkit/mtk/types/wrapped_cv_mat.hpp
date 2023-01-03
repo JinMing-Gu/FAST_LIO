cb1fa88a0764ffe39902869429de1e9401629fa3
gjm10775 <jinming.gu@uisee.com>
2023-01-03 17:22:11 +0800

    code format

--- include/IKFoM_toolkit/mtk/types/wrapped_cv_mat.hpp
+++ include/IKFoM_toolkit/mtk/types/wrapped_cv_mat.hpp
@@ -15,9 +15,9 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of the Universitaet Bremen nor the DFKI GmbH 
- *     nor the names of its contributors may be used to endorse or 
- *     promote products derived from this software without specific 
+ *   * Neither the name of the Universitaet Bremen nor the DFKI GmbH
+ *     nor the names of its contributors may be used to endorse or
+ *     promote products derived from this software without specific
  *     prior written permission.
  *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -40,73 +40,83 @@
 #include <Eigen/Core>
 #include <opencv/cv.h>
 
-namespace MTK {
-
-template<class f_type>
-struct cv_f_type;
-
-template<>
-struct cv_f_type<double>
+namespace MTK
 {
-	enum {value = CV_64F};
-};
 
-template<>
-struct cv_f_type<float>
-{
-	enum {value = CV_32F};
-};
+    template <class f_type>
+    struct cv_f_type;
 
-/**
- * cv_mat wraps a CvMat around an Eigen Matrix
- */
-template<int rows, int cols, class f_type = double>
-class cv_mat : public matrix<rows, cols, f_type, cols==1 ? Eigen::ColMajor : Eigen::RowMajor>
-{
-	typedef matrix<rows, cols, f_type, cols==1 ? Eigen::ColMajor : Eigen::RowMajor> base_type;
-	enum {type_ = cv_f_type<f_type>::value};
-	CvMat cv_mat_;
+    template <>
+    struct cv_f_type<double>
+    {
+        enum
+        {
+            value = CV_64F
+        };
+    };
 
-public:
-	cv_mat()
-	{
-		cv_mat_ = cvMat(rows, cols, type_, base_type::data());
-	}
+    template <>
+    struct cv_f_type<float>
+    {
+        enum
+        {
+            value = CV_32F
+        };
+    };
 
-	cv_mat(const cv_mat& oth) : base_type(oth)
-	{
-		cv_mat_ = cvMat(rows, cols, type_, base_type::data());
-	}
+    /**
+     * cv_mat wraps a CvMat around an Eigen Matrix
+     */
+    template <int rows, int cols, class f_type = double>
+    class cv_mat : public matrix<rows, cols, f_type, cols == 1 ? Eigen::ColMajor : Eigen::RowMajor>
+    {
+        typedef matrix<rows, cols, f_type, cols == 1 ? Eigen::ColMajor : Eigen::RowMajor> base_type;
+        enum
+        {
+            type_ = cv_f_type<f_type>::value
+        };
+        CvMat cv_mat_;
 
-	template<class Derived>
-	cv_mat(const Eigen::MatrixBase<Derived> &value) : base_type(value)
-	{
-		cv_mat_ = cvMat(rows, cols, type_, base_type::data());
-	}
+    public:
+        cv_mat()
+        {
+            cv_mat_ = cvMat(rows, cols, type_, base_type::data());
+        }
 
-	template<class Derived>
-	cv_mat& operator=(const Eigen::MatrixBase<Derived> &value)
-	{
-		base_type::operator=(value);
-		return *this;
-	}
+        cv_mat(const cv_mat &oth) : base_type(oth)
+        {
+            cv_mat_ = cvMat(rows, cols, type_, base_type::data());
+        }
 
-	cv_mat& operator=(const cv_mat& value)
-	{
-		base_type::operator=(value);
-		return *this;
-	}
-	
-	// FIXME: Maybe overloading operator& is not a good idea ...
-	CvMat* operator&()
-	{
-		return &cv_mat_;
-	}
-	const CvMat* operator&() const
-	{
-		return &cv_mat_;
-	}
-};
+        template <class Derived>
+        cv_mat(const Eigen::MatrixBase<Derived> &value) : base_type(value)
+        {
+            cv_mat_ = cvMat(rows, cols, type_, base_type::data());
+        }
+
+        template <class Derived>
+        cv_mat &operator=(const Eigen::MatrixBase<Derived> &value)
+        {
+            base_type::operator=(value);
+            return *this;
+        }
+
+        cv_mat &operator=(const cv_mat &value)
+        {
+            base_type::operator=(value);
+            return *this;
+        }
+
+        // FIXME: Maybe overloading operator& is not a good idea ...
+        CvMat *operator&()
+        {
+            return &cv_mat_;
+        }
+        const CvMat *operator&() const
+        {
+            return &cv_mat_;
+        }
+    };
 
 } // namespace MTK
 
